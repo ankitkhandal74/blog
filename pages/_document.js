9def30a27339ec260a12527540abb5a39127afe8
Ankit khandal <160460793+ankitkhandal74@users.noreply.github.com>
2024-10-08 18:42:26 +0530

    _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,17 @@ export default function MyDocument() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Karla:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
-
+                <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "offl5jgb0g");
+            `,
+          }}
+        />
 
                 {/* Favicon (Logo) */}
                 <link rel="icon" href="/logo2.png" />
