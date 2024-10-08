5b899b06de141b8bd30d3bd7256610fc8f8bf57e
Ankit khandal <160460793+ankitkhandal74@users.noreply.github.com>
2024-10-08 18:45:48 +0530

    _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,18 @@ export default function MyDocument() {
             `,
           }}
         />
+              <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "offl5jgb0g");
+            `,
+          }}
+        />
 
                 {/* Favicon (Logo) */}
                 <link rel="icon" href="/logo2.png" />
