cb25f97379024aa8858437d7cd313b9ee6a0e069
Ankit khandal <160460793+ankitkhandal74@users.noreply.github.com>
2025-01-31 11:30:45 +0530

    _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function MyDocument() {
     return (
@@ -57,6 +58,7 @@ export default function MyDocument() {
             <body>
                 <Main />
                 <NextScript />
+                <SpeedInsights />
             </body>
         </Html>
     );
